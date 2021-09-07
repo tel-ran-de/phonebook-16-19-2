4beaaafc2bc5f3938282885b3409b6e90c46ed9b
Jürgen Jurk <62541560+Juergen-J@users.noreply.github.com>
2021-09-07 12:06:07 +0200

    Pb 11 (#6)

---
+++ phonebook-api/src/main/java/com/telran/phonebookapi/entity/Phone.java
@@ -0,0 +1,44 @@
+package com.telran.phonebookapi.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Phone {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Setter
+    private String countryCode;
+    @Setter
+    private String telephoneNumber;
+    @Setter
+    private boolean isFavorite;
+
+    @ManyToOne
+    private Contact contact;
+
+    public Phone(@NonNull String countryCode, @NonNull String telephoneNumber, boolean isFavorite, @NonNull Contact contact) {
+        this.countryCode = countryCode;
+        this.telephoneNumber = telephoneNumber;
+        this.isFavorite = isFavorite;
+        this.contact = contact;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Phone)) return false;
+        Phone phone = (Phone) o;
+        return Objects.equals(id, phone.id) && Objects.equals(countryCode, phone.countryCode) && Objects.equals(telephoneNumber, phone.telephoneNumber);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, countryCode, telephoneNumber);
+    }
+}
