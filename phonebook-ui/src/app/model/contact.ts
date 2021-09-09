69fdb01b232820c3c87c00ed9464a73f66190a7f
Anastasia Kosova <82057819+Akokoc@users.noreply.github.com>
2021-09-09 11:13:14 +0200

    PB-17 (#13)

---
+++ phonebook-ui/src/app/model/contact.ts
@@ -0,0 +1,9 @@
+export interface Contact {
+  id : number;
+  firstName: string;
+  lastName: string;
+  age: number;
+  isFavorite: boolean;
+  group: string;
+}
+
---
+++ phonebook-ui/src/app/model/phone.ts
@@ -0,0 +1,7 @@
+export interface Phone {
+  id : number;
+  countryCode: string;
+  telephoneNumber: string;
+  isFavorite: boolean;
+  contactId: number;
+}
