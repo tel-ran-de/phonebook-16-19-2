1faf8465dc754ee9c22429c608f24130a50fdfd9
Akokoc <a.a.kokoc@gmail.com>
2021-09-09 09:51:10 +0200

    Add model interfaces to ui

---
+++ phonebook-ui/src/app/model/address.ts
@@ -0,0 +1,10 @@
+export interface Address {
+  id : number;
+  country: string;
+  city: string;
+  index: string;
+  street: string;
+  homeNr: number;
+  isFavorite: boolean;
+  contactId: number;
+}
