c05c1ecd8cfbcd66e9ca216386054a50cb7df152
Juergen-J <yurk.jur@gmail.com>
2021-09-14 09:21:31 +0200

    PB-20 navigation updated

--- phonebook-ui/src/app/app-routing.module.ts
+++ phonebook-ui/src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import {AppComponent} from "./app.component";
 
 
 const routes: Routes = [
-  { path: '/', component: AppComponent}
+
 ];
 
 @NgModule({
--- phonebook-ui/src/app/navigation/navigation.component.ts
+++ phonebook-ui/src/app/navigation/navigation.component.ts
@@ -5,11 +5,6 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './navigation.component.html',
   styleUrls: ['./navigation.component.css']
 })
-export class NavigationComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+export class NavigationComponent  {
 
 }
