dc52bef8f8eb2af1956b24cb85148079a5361e55
Anastasia Kosova <82057819+Akokoc@users.noreply.github.com>
2021-09-20 11:06:23 +0200

    pb-28-3 (#25)

---
+++ phonebook-api/src/main/java/com/telran/phonebookapi/dto/AddressDto.java
@@ -0,0 +1,25 @@
+package com.telran.phonebookapi.dto;
+
+
+public class AddressDto {
+    public Long id;
+    public String country;
+    public String city;
+    public String index;
+    public String street;
+    public String homeNr;
+    public boolean isFavorite;
+    public Long contactId;
+
+    public AddressDto(Long id, String country, String city, String index, String street, String homeNr, boolean isFavorite, Long contactId) {
+        this.id = id;
+        this.country = country;
+        this.city = city;
+        this.index = index;
+        this.street = street;
+        this.homeNr = homeNr;
+        this.isFavorite = isFavorite;
+        this.contactId = contactId;
+    }
+
+}
